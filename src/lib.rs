27c8b8665422df2a55099e350396210bca0d0c5d
Moritz ‎‎‎‎‎‎‎‎ <moluecke@gmx.de>
2023-03-18 15:46:56 +0100

    attrib pointers moved to Vertex struct

--- src/lib.rs
+++ src/lib.rs
@@ -62,28 +62,8 @@ pub fn test() -> Result<(), err::Error> {
         gl.BindVertexArray(vao);
         gl.BindBuffer(gl::ARRAY_BUFFER, vbo);
         
-        // Position
-        gl.EnableVertexAttribArray(0);
-        gl.VertexAttribPointer(
-            0,               // Index of attribute
-            3,               // Number of components per attribute
-            gl::FLOAT,       // Data type
-            gl::FALSE,       // Normalized (int to float conversion)
-            (7 * std::mem::size_of::<f32>()) as gl::types::GLint,  // Byte offset between consecutive attributes
-            std::ptr::null() // Offset of first component
-        );
-
-        // Color 
-        gl.EnableVertexAttribArray(1);
-        gl.VertexAttribPointer(
-            1,               // Index of attribute
-            4,               // Number of components per attribute
-            gl::FLOAT,       // Data type
-            gl::FALSE,       // Normalized (int to float conversion)
-            (7 * std::mem::size_of::<f32>()) as gl::types::GLint,           // Byte offset between consecutive attributes
-            (3 * std::mem::size_of::<f32>()) as *const gl::types::GLvoid    // Offset of first component
-        );
-        
+        Vertex::attrib_pointers(&gl);
+                
         gl.BindBuffer(gl::ARRAY_BUFFER, 0);
         gl.BindVertexArray(0);
     }
--- src/render/vertex.rs
+++ src/render/vertex.rs
@@ -6,3 +6,30 @@ pub struct Vertex {
     pub color: (f32, f32, f32, f32)
 }
 
+impl Vertex {
+    pub fn attrib_pointers(gl: &gl::Gl) {
+        unsafe {
+            // Position
+            gl.EnableVertexAttribArray(0);
+            gl.VertexAttribPointer(
+                0,               // Index of attribute
+                3,               // Number of components per attribute
+                gl::FLOAT,       // Data type
+                gl::FALSE,       // Normalized (int to float conversion)
+                (std::mem::size_of::<Self>()) as gl::types::GLint,  // Byte offset between consecutive attributes
+                std::ptr::null() // Offset of first component
+            );
+
+            // Color 
+            gl.EnableVertexAttribArray(1);
+            gl.VertexAttribPointer(
+                1,               // Index of attribute
+                4,               // Number of components per attribute
+                gl::FLOAT,       // Data type
+                gl::FALSE,       // Normalized (int to float conversion)
+                (std::mem::size_of::<Self>()) as gl::types::GLint,              // Byte offset between consecutive attributes
+                (3 * std::mem::size_of::<f32>()) as *const gl::types::GLvoid    // Offset of first component
+            );
+        }
+    }
+}
