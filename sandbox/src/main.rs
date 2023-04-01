4e24459d0005813bc94a96facbebdf6652b3b14a
Moritz ‎‎‎‎‎‎‎‎ <moluecke@gmx.de>
2023-04-01 16:47:46 +0200

    sandbox example

--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -3,15 +3,11 @@
 fn main() -> visim::Result<()> {
     let mut window = visim::Window::new("Hello world", 1280, 720)?;
 
-    let mut input = String::new();
-    std::io::stdin().read_line(&mut input).unwrap();
-    let trimmed = input.trim();
-    let x = match trimmed.parse::<f32>() {
-        Ok(f) => f,
-        Err(..) => panic!()
-    };
-
-    let mut t = visim::shapes::Polygon::new(&window, &[visim::Vertex { pos: (x, 0.5, 0.0), color: (1.0, 1.0, 0.0, 1.0) }])?;
+    let mut t = visim::shapes::Polygon::new(&window, &[
+        visim::Vertex::new(visim::Vector2::new(-0.5, -0.5), visim::Color::rgb(1.0, 0.0, 0.0)),
+        visim::Vertex::new(visim::Vector2::new(0.5, -0.5), visim::Color::rgb(0.0, 1.0, 0.0)),
+        visim::Vertex::new(visim::Vector2::new(0.0, 0.5), visim::Color::rgb(0.0, 0.0, 1.0)),
+    ])?;
     t.fill = true;   
 
     while window.is_open() {
