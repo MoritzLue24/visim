bb31b0d1d2f068f910bb959333905fe194eaee54
Moritz ‎‎‎‎‎‎‎‎ <moluecke@gmx.de>
2023-04-01 16:43:02 +0200

    Color constructors

--- src/color.rs
+++ src/color.rs
@@ -7,3 +7,13 @@ pub struct Color {
     b: f32,
     a: f32
 }
+
+impl Color {
+    pub fn rgba(r: f32, g: f32, b: f32, a: f32) -> Color {
+        Self { r, g, b, a }
+    }
+
+    pub fn rgb(r: f32, g: f32, b: f32) -> Color {
+        Self { r, g, b, a: 1.0 }
+    }
+}
