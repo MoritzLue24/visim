2aed11f5eb336d641be224d76b31d300756763e2
Moritz ‎‎‎‎‎‎‎‎ <moluecke@gmx.de>
2023-04-01 16:39:54 +0200

    Vertex attrib pointer fixed & changed to 2d

--- src/render/vertex.rs
+++ src/render/vertex.rs
@@ -19,7 +19,7 @@ impl Vertex {
             gl.EnableVertexAttribArray(0);
             gl.VertexAttribPointer(
                 0,               // Index of attribute
-                3,               // Number of components per attribute
+                2,               // Number of components per attribute
                 gl::FLOAT,       // Data type
                 gl::FALSE,       // Normalized (int to float conversion)
                 (std::mem::size_of::<Self>()) as gl::types::GLint,  // Byte offset between consecutive attributes
@@ -34,7 +34,7 @@ impl Vertex {
                 gl::FLOAT,       // Data type
                 gl::FALSE,       // Normalized (int to float conversion)
                 (std::mem::size_of::<Self>()) as gl::types::GLint,              // Byte offset between consecutive attributes
-                (3 * std::mem::size_of::<f32>()) as *const gl::types::GLvoid    // Offset of first component
+                (std::mem::size_of::<Vector2>()) as *const gl::types::GLvoid    // Offset of first component
             );
         }
     }
