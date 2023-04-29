7b2c50172cc45a12bdb3d390e1d33b499a03f5f8
Moritz ‎‎‎‎‎‎‎‎ <moluecke@gmx.de>
2023-04-29 16:37:28 +0200

    Renderer::polygon fn (doesnt work due to invalid operation at Vertex::attrib_pointers second attribute (color))

--- src/renderer.rs
+++ src/renderer.rs
@@ -5,9 +5,10 @@ pub struct Renderer {
     _sdl_gl_ctx: sdl2::video::GLContext,
     gl: gl::Gl,
     program: Program,
-    vbo: buffer::Array,
+    pub vbo: buffer::Array<Vertex>,
+    ibo: buffer::ElementArray<u32>,
     vao: VertexArray,
-    ibo: buffer::ElementArray
+    last_ibo_i: i32
 }
 
 impl Renderer {
@@ -15,12 +16,15 @@ impl Renderer {
         let gl_ctx = window.gl_create_context().map_err(|e| err::new(e))?;
         let gl = gl::load_with(|s| window.subsystem().gl_get_proc_address(s) as _);
 
-        Vertex::attrib_pointers(&gl);
         let program = Program::default(&gl)?;
         let vbo = buffer::Array::new(&gl, buffer::DrawUsage::Dynamic);
-        let vao = VertexArray::new(&gl);
         let ibo = buffer::ElementArray::new(&gl, buffer::DrawUsage::Dynamic);
-        
+        let vao = VertexArray::new(&gl);
+
+        vao.bind();
+        Vertex::attrib_pointers(&gl);
+        vao.unbind();
+
         let win_size = window.size();
         unsafe {
             gl.Viewport(0, 0,
@@ -29,16 +33,26 @@ impl Renderer {
             );
         };
 
-        Ok(Self { _sdl_gl_ctx: gl_ctx, gl, program, vbo, vao, ibo })
+        Ok(Self { 
+            _sdl_gl_ctx: gl_ctx,
+            gl,
+            program,
+            vbo,
+            ibo,
+            vao,
+            last_ibo_i: 0
+        })
     }
 
     pub fn render(&self) {
-
+        self.program.bind();
+        self.vao.bind();
+        unsafe {self.gl.DrawElements(gl::TRIANGLES, self.ibo.len(), gl::UNSIGNED_INT, 0 as *const std::ffi::c_void) }
     }
 
     pub fn clear<C: Into<Color>>(&self, color: C) {
-        self.vbo.write_data::<Vertex>(&[]);
-        self.ibo.write_data::<i32>(&[]);
+        self.vbo.write_data(&[]);
+        self.ibo.write_data(&[]);
     
         let col: Color = color.into();
 
@@ -46,5 +60,15 @@ impl Renderer {
             self.gl.ClearColor(col.r, col.g, col.b, col.a);
             self.gl.Clear(gl::COLOR_BUFFER_BIT);
         }
+    }
+
+    pub fn polygon<V: Into<Vertex>>(&self, vertices: Vec<V>) {
+        let mut data = Vec::<Vertex>::new();
+        for vertex in vertices {
+            data.push(vertex.into());
+        }
+
+        self.vbo.write_data(&data);
+        self.ibo.write_data(&[0, 1, 2]);
     }
 }
