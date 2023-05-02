d7a3c13c11b70b5b0185f5d674dba98711253ccd
Moritz ‎‎‎‎‎‎‎‎ <moluecke@gmx.de>
2023-05-02 20:29:19 +0200

    Dynamic geometry test

--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -2,24 +2,25 @@
 
 fn main() -> visim::Result<()> {
     let mut window = visim::Window::new("Hello world", 1280, 720)?;
-    
+    let mut x = 0.0;
+        
     while window.is_open() {
-        for event in window.get_events() {
-            match event {
-                visim::Event::Quit { .. } => println!("quit"),
-                visim::Event::KeyDown { .. } => println!("{:?}", event),
-                _ => ()
-            }
-        }
-    
+        window.get_events();
         window.renderer.clear((0.5, 0.2, 0.7, 1.0));
+        
+        x += 0.001;
         window.renderer.polygon(vec![
-            ((-0.5, 0.0), (1.0, 0.0, 0.0)),
-            ((0.0, 0.5), (0.0, 1.0, 0.0)),
-            ((0.5, 0.0), (0.0, 0.0, 1.0))
+            ((-1.0, 0.0), (1.0, 0.0, 0.0)),
+            ((-0.5, 0.5), (0.0, 1.0, 0.0)),
+            ((0.0, 0.0), (0.0, 0.0, 1.0))
         ]);
+        window.renderer.polygon(vec![
+            ((0.0 + x, 0.0), (1.0, 0.0, 0.0)),
+            ((0.5 + x, 0.5), (0.0, 1.0, 0.0)),
+            ((1.0 + x, 0.0), (0.0, 0.0, 1.0))
+        ]);
+
         window.update();
-        break;
     }
 
     Ok(())
