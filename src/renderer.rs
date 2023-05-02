9173d0f151805252ed63df64546825d6d556537b
Moritz ‎‎‎‎‎‎‎‎ <moluecke@gmx.de>
2023-05-02 20:28:33 +0200

    Buffer::append_data used for dynamic geometry

--- src/renderer.rs
+++ src/renderer.rs
@@ -2,7 +2,8 @@ use crate::{render::{buffer, VertexArray}, Result, Program, err, Vertex, Color};
 
 
 pub struct Renderer {
-    last_ibo_i: u32,
+    vertices_len: u32,
+    max_vertices: isize,
 
     gl: gl::Gl,
     program: Program,
@@ -16,14 +17,14 @@ pub struct Renderer {
 }
 
 impl Renderer {
-    pub fn new(window: &sdl2::video::Window) -> Result<Self> {
+    pub fn new(window: &sdl2::video::Window, max_vertices: isize) -> Result<Self> {
         let gl_ctx = window.gl_create_context().map_err(|e| err::new(e))?;
         let gl = gl::load_with(|s| window.subsystem().gl_get_proc_address(s) as _);
 
         let program = Program::default(&gl)?;
         let vao = VertexArray::new(&gl);
-        let vbo = buffer::Array::new(&gl, buffer::DrawUsage::Dynamic);
-        let ibo = buffer::ElementArray::new(&gl, buffer::DrawUsage::Dynamic);
+        let mut vbo = buffer::Array::new(&gl, buffer::DrawUsage::Dynamic);
+        let mut ibo = buffer::ElementArray::new(&gl, buffer::DrawUsage::Dynamic);
 
         vao.bind();
         vbo.bind();
@@ -32,6 +33,9 @@ impl Renderer {
         vao.unbind();
         vbo.unbind();
         ibo.unbind();
+
+        vbo.allocate_data(std::mem::size_of::<Vertex>() as isize * max_vertices);
+        ibo.allocate_data(std::mem::size_of::<u32>() as isize * max_vertices);
  
         let win_size = window.size();
         unsafe {
@@ -42,7 +46,7 @@ impl Renderer {
         };
 
         Ok(Self { 
-            last_ibo_i: 0,
+            vertices_len: 0, max_vertices,
             gl, program, vbo, ibo, vao,
             _sdl_gl_ctx: gl_ctx,
         })
@@ -51,14 +55,15 @@ impl Renderer {
     pub fn render(&self) {
         self.program.bind();
         self.vao.bind();
-        unsafe {self.gl.DrawElements(gl::TRIANGLES, self.vbo.len(), gl::UNSIGNED_INT, 0 as *const std::ffi::c_void) }
+        unsafe {self.gl.DrawElements(gl::TRIANGLES, self.vertices_len as i32, gl::UNSIGNED_INT, 0 as *const std::ffi::c_void) }
         self.vao.unbind();
     }
 
-    pub fn clear<C: Into<Color>>(&self, color: C) {
-        self.vbo.write_data(&[]);
-        self.ibo.write_data(&[]);
-    
+    pub fn clear<C: Into<Color>>(&mut self, color: C) {
+        self.vertices_len = 0;
+        self.vbo.allocate_data(std::mem::size_of::<Vertex>() as isize * self.max_vertices);
+        self.ibo.allocate_data(std::mem::size_of::<u32>() as isize * self.max_vertices);
+
         let col: Color = color.into();
 
         unsafe {
@@ -69,17 +74,17 @@ impl Renderer {
 
     pub fn polygon<V: Into<Vertex>>(&mut self, vertices: Vec<V>) {
         let data: Vec<Vertex> = vertices.into_iter().map(|v| v.into()).collect();
-        self.vbo.write_data(&data);
+        self.vbo.append_data(&data);
 
         // Implement an algorithm that creates indices for
         // triangles based on the vertices.
-        todo!();
-
-        // let indices = vec![0, 1, 2];
         // for _ in 0..data.len() {
         //     indices.push(self.last_ibo_i);
         //     self.last_ibo_i += 1;
         // }
-        // self.ibo.write_data(&indices);
+
+        let indices = vec![self.vertices_len, self.vertices_len + 1, self.vertices_len + 2];
+        self.ibo.append_data(&indices);
+        self.vertices_len += data.len() as u32;
     }
 }
