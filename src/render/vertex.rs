53f8f91ad9dd8a9ff5702042d1a4996923e872fa
Moritz ‎‎‎‎‎‎‎‎ <moluecke@gmx.de>
2023-05-21 21:34:53 +0200

    Vertex attributes for textures

--- src/render/vertex.rs
+++ src/render/vertex.rs
@@ -4,12 +4,14 @@ use crate::{Vector2, Color};
 #[repr(C, packed)]
 pub struct Vertex {
     pub pos: Vector2,
-    pub color: Color
+    pub color: Color,
+    pub tex_id: f32,
+    pub tex_coords: Vector2,
 }
 
 impl Vertex {
-    pub fn new<P: Into<Vector2>, C: Into<Color>>(pos: P, color: C) -> Self {
-        Self { pos: pos.into(), color: color.into() }
+    pub fn new<V: Into<Vector2>, C: Into<Color>>(pos: V, color: C, tex_id: f32, tex_coords: V) -> Self {
+        Self { pos: pos.into(), color: color.into(), tex_id, tex_coords: tex_coords.into() }
     }
 
     pub fn attrib_pointers(gl: &gl::Gl, vao_id: gl::types::GLuint) {
@@ -37,13 +39,37 @@ impl Vertex {
             );
 
             // Texture id 
+            gl.EnableVertexArrayAttrib(vao_id, 2);
+            gl.VertexAttribPointer(
+                2,
+                1,
+                gl::FLOAT,
+                gl::FALSE,
+                std::mem::size_of::<Self>() as gl::types::GLint,
+                (std::mem::size_of::<Vector2>() + std::mem::size_of::<Color>()) as *const gl::types::GLvoid
+            );
+
+            // Texture coordinates
+            gl.EnableVertexArrayAttrib(vao_id, 3);
+            gl.VertexAttribPointer(
+                3,
+                2,
+                gl::FLOAT,
+                gl::FALSE,
+                std::mem::size_of::<Self>() as gl::types::GLint,
+                (
+                    std::mem::size_of::<Vector2>() + 
+                    std::mem::size_of::<Color>() +
+                    std::mem::size_of::<f32>()
+                ) as *const gl::types::GLvoid
+            );
         }
     }
 }
 
-impl<P: Into<Vector2>, C: Into<Color>> From<(P, C)> for Vertex {
-    fn from(value: (P, C)) -> Self {
-        Self { pos: value.0.into(), color: value.1.into() }
+impl<V: Into<Vector2>, C: Into<Color>> From<(V, C, f32, V)> for Vertex {
+    fn from(value: (V, C, f32, V)) -> Self {
+        Self { pos: value.0.into(), color: value.1.into(), tex_id: value.2, tex_coords: value.3.into() }
     }
 }
 
